35091a9ee133c98afbdf378b926b538721d6f39f
Alessandro <alessandro.maggio98@gmail.com>
2020-04-20 01:40:21 +0200

    testing

---
+++ src/tests/AzioneTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AzioneTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void updateValue() {
+    }
+}
---
+++ src/tests/PatrimonioTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PatrimonioTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getTotalAmount() {
+    }
+
+    @Test
+    void startAutomaticMenagement() {
+    }
+}
